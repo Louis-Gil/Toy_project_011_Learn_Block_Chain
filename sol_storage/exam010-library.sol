c802ddd907764f48d1b13346dc75a0532061270a
Gilin3000 <lotus0110@nate.com>
2023-03-22 14:47:02 +0900

    docs: sol library

---
+++ sol_storage/exam010-library.sol
@@ -0,0 +1,19 @@
+//SPDX-license-Identifier : GPL-30
+pragma solidity >= 0.7.0 < 0.9.0;
+
+library safeMatch{ 
+	function add(uint8 a, uint8 b) internal pure returns (uint8) {
+		require(a+b >= a, "safeMatch : addition overflow");
+		return a + b;
+	}
+}
+
+contract exam{
+	using safeMatch for uint8;
+	uint8 public a;
+
+	function becomeOverFlow(uint8 _num1, uint8 _num2) public {
+		a = _num1.add(_num2);
+		//a = safeMatch.add(_num1, _num2);
+	}
+}
